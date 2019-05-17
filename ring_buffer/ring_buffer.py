384642a13b99b61a988d7a1d17e6ae5f9a6936e8
Casey Bee <caseye.baker@gmail.com>
2019-05-17 08:24:53 -0700

    ring buffer test is passing

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -5,7 +5,57 @@ class RingBuffer:
     self.storage = [None]*capacity
 
   def append(self, item):
+    length = len(self.storage)
+    #if the current storage is at capacity
+    for i in range (0, length):
+        if self.storage[i] == None:
+            self.storage[i] = item
+            print(f"for i self.storage: {self.storage}")
+            return
+    #append to current oldest index
+    self.storage[self.current] = item
+    #next index is now the oldest
+    if self.current == self.capacity-1:
+        self.current = 0
+    else:
+        self.current += 1    
     pass
 
   def get(self):
+    # have a result array
+    result = []
+    # for each element
+    for element in self.storage:
+        # check that element is not none
+        if element is not None:
+            # append to result array
+            result.append(element)
+    # return array
+    return result
     pass
+
+buffer = RingBuffer(3)
+
+print(buffer.get())   # should return []
+
+buffer.append('a')
+buffer.append('b')
+buffer.append('c')
+
+print(buffer.get())   # should return ['a', 'b', 'c']
+
+# 'd' overwrites the oldest value in the ring buffer, which is 'a'
+buffer.append('d')
+
+print(buffer.get())   # should return ['d', 'b', 'c']
+
+buffer.append('e')
+buffer.append('f')
+
+print(buffer.get())   # should return ['d', 'e', 'f']
+
+buffer.append('g')
+buffer.append('h')
+buffer.append('i')
+
+print(buffer.get())
